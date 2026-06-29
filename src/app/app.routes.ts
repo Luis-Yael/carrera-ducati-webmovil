d19ea0b1b33fbfaaa892798e8780f4914a858ade
Luis Yael Mendez Sanchez <luisyaelms@gmail.com>
2026-06-29 11:24:28 -0600

    Home Screen
    Se programa la pantalla del home de la app con todos sus elementos.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { LoginScreen } from './screens/login-screen/login-screen';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -17,6 +16,22 @@ export const routes: Routes = [
       {
         path: 'home',
         loadComponent: () => import('./screens/home-screen/home-screen').then(m => m.HomeScreen),
+      },
+      {
+        path: 'instrucciones',
+        loadComponent: () => import('./screens/instrucciones-screen/instrucciones-screen').then(m => m.InstruccionesScreen),
+      },
+      {
+        path: 'bases-promocion',
+        loadComponent: () => import('./screens/bases-promocion-screen/bases-promocion-screen').then(m => m.BasesPromocionScreen),
+      },
+      {
+        path: 'politica-privacidad',
+        loadComponent: () => import('./screens/politica-privacidad-screen/politica-privacidad-screen').then(m => m.PoliticaPrivacidadScreen),
+      },
+      {
+        path: 'terminos-condiciones',
+        loadComponent: () => import('./screens/terminos-condiciones-screen/terminos-condiciones-screen').then(m => m.TerminosCondicionesScreen),
       }
     ]
   }
